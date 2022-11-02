e4c19024d3304ed2b666d9022614ef3b015ca06e
asantentata <45817522+asantentata@users.noreply.github.com>
2022-11-02 13:04:41 +0000

    Update bb_pipeline_diff.py
    Cleaner version with NODDI_cuDIMOT and no autopPtx

--- bb_diffusion_pipeline/bb_pipeline_diff.py
+++ bb_diffusion_pipeline/bb_pipeline_diff.py
@@ -36,14 +36,14 @@ def bb_pipeline_diff(subject, jobHold, fileConfiguration, GDC_Status, Machine):
     jobPREPARE =      LT.runCommand(logger, '${FSLDIR}/bin/fsl_sub -T 5   -N "bb_pre_eddy_'            + subject + '" -j ' + jobHold        + '  -l ' + logDir + ' $BB_BIN_DIR/bb_diffusion_pipeline/bb_eddy/bb_pre_eddy ' + subject )
     jobEDDY =         LT.runCommand(logger, '${FSLDIR}/bin/fsl_sub -T 75  -N "bb_eddy_'                + subject + '" -j ' + jobPREPARE     + '  -q $FSLGECUDAQ -l ' + logDir + ' $BB_BIN_DIR/bb_diffusion_pipeline/bb_eddy/bb_eddy_wrap ' + baseDir )
     jobPOSTEDDY =     LT.runCommand(logger, '${FSLDIR}/bin/fsl_sub -T 60  -N "bb_post_eddy_'           + subject + '" -j ' + jobEDDY        + '  -l ' + logDir + ' $BB_BIN_DIR/bb_diffusion_pipeline/bb_eddy/bb_post_eddy ' + baseDir + ' ' + GDC_Status + ' ' + Machine )
+    jobPOSTEDDY =     LT.runCommand(logger, '${FSLDIR}/bin/fsl_sub -T 60  -N "bb_post_eddy_'           + subject + '"'      + ' -l ' + logDir + ' $BB_BIN_DIR/bb_diffusion_pipeline/bb_eddy/bb_post_eddy ' + baseDir + ' ' + GDC_Status + ' ' + Machine )
     jobDTIFIT =       LT.runCommand(logger, '${FSLDIR}/bin/fsl_sub -T 5   -N "bb_dtifit_'              + subject + '" -j ' + jobPOSTEDDY    + '  -l ' + logDir + ' ${FSLDIR}/bin/dtifit -k ' + baseDir + '/dMRI/dMRI/data_ud_1_shell -m ' + baseDir + '/dMRI/dMRI/nodif_brain_mask_ud -r ' + baseDir + '/dMRI/dMRI/data_ud_1_shell.bvec -b ' + baseDir + '/dMRI/dMRI/data_ud_1_shell.bval -o ' + baseDir + '/dMRI/dMRI/dti')
     jobTBSS =         LT.runCommand(logger, '${FSLDIR}/bin/fsl_sub -T 240 -N "bb_tbss_'                + subject + '" -j ' + jobDTIFIT      + '  -l ' + logDir + ' $BB_BIN_DIR/bb_diffusion_pipeline/bb_tbss/bb_tbss_general ' + subject )
     jobPREBEDPOSTX =  LT.runCommand(logger, '${FSLDIR}/bin/fsl_sub -T 5   -N "bb_pre_bedpostx_gpu_'    + subject + '" -j ' + jobDTIFIT      + '  -l ' + logDir + ' $BB_BIN_DIR/bb_diffusion_pipeline/bb_bedpostx/bb_pre_bedpostx_gpu ' + baseDir + '/dMRI/dMRI')
     jobBEDPOSTX =     LT.runCommand(logger, '${FSLDIR}/bin/fsl_sub -T 190 -N "bb_bedpostx_gpu_'        + subject + '" -j ' + jobPREBEDPOSTX + '  -q $FSLGECUDAQ -l ' + logDir + ' $BB_BIN_DIR/bb_diffusion_pipeline/bb_bedpostx/bb_bedpostx_gpu ' + baseDir + '/dMRI/dMRI')
     jobPOSTBEDPOSTX = LT.runCommand(logger, '${FSLDIR}/bin/fsl_sub -T 15  -N "bb_post_bedpostx_gpu_'   + subject + '" -j ' + jobBEDPOSTX    + '  -l ' + logDir + ' $BB_BIN_DIR/bb_diffusion_pipeline/bb_bedpostx/bb_post_bedpostx_gpu ' + baseDir + '/dMRI/dMRI')
-    jobNODDI=         LT.runCommand(logger,  '${FSLDIR}/bin/fsl_sub -T 60 -N "bb_NODDI_cuDIMOT_' + subject + '" -j ' + jobPOSTBEDPOSTX + ' -l ' + logDir + ' $BB_BIN_DIR/bb_diffusion_pipeline/bb_NODDI_cuDIMOT/bb_NODDI_cuDIMOT ' + subject )
-    #jobNODDI=         LT.runCommand(logger, '${FSLDIR}/bin/fsl_sub -T 100 -N "bb_NODDI_'               + subject + '" -j ' + jobTBSS        + '  -l ' + logDir + ' $BB_BIN_DIR/bb_diffusion_pipeline/bb_NODDI ' + subject )
-    #jobAUTOPTX =      LT.runCommand(logger, '$BB_BIN_DIR/bb_diffusion_pipeline/bb_autoPtx/bb_autoPtx ' + subject + ' ' + jobPOSTBEDPOSTX+','+jobTBSS)
+    jobNODDI=         LT.runCommand(logger,  '${FSLDIR}/bin/fsl_sub -T 60 -N "bb_NODDI_cuDIMOT_' + subject + '"' + ' -l ' + logDir + ' $BB_BIN_DIR/bb_diffusion_pipeline/bb_NODDI_cuDIMOT/bb_NODDI_cuDIMOT ' + subject )
     jobXTRACT =      LT.runCommand(logger, '$BB_BIN_DIR/bb_diffusion_pipeline/bb_xtract/bb_xtract ' + subject + ' ' + jobNODDI+','+jobTBSS)
 
+    
     return jobXTRACT
